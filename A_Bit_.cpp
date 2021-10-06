05ecae70ae81cf473f967b2c86a6d520a536a4d7
RAHUL <rahulsaini13900@gmail.com>
2021-10-06 15:02:25 +0530

    first change

---
+++ A_Bit_.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    cout << "hello world\n";
+    // int t ; 
+    // cin >> t ; 
+    // while(t--){
+    //     int n ;
+    //      cin >> n ; 
+
+    //      int res = 0;
+
+    //     for(int i = 0 ; i < n ; i++){
+    //         string s;
+    //         cin >> s ;
+
+    //         if(s == "++X" || s == "X++") res++;
+    //         else if(s == "--X" || s == "X--") res--;
+    //     }
+    //     cout << res << "\n" ;
+    //     }
+}
